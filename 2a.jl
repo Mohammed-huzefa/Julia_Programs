5d7e151a031de6a12c503745110c3cbffe7d78a0
Akshay Srivatsava. B <86884558+sriVaatsavaa6@users.noreply.github.com>
2026-05-24 19:26:07 +0530

    Add makepay function to calculate total payment

---
+++ 2a.jl
@@ -0,0 +1,8 @@
+function makepay()
+print("Enter the number of hours you want to pay:")
+hours = parse(Int,readline())
+print("Cost of parts:")
+cost=parse(Int,readline())
+tot=max(100*hours+cost,150)
+print(tot)
+end

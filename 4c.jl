3ed6cd3422edd86e0bddd74449c0f73dc7838cd8
Akshay Srivatsava. B <86884558+sriVaatsavaa6@users.noreply.github.com>
2026-05-24 20:57:00 +0530

    u

---
+++ 4c.jl
@@ -0,0 +1,14 @@
+begin
+function fibonacci(n)
+if n<=1
+return 1
+else
+return fibonacci(n-1) + fibonacci(n-2)
+end
+end
+println("Enter a number:")
+n=parse(Int64,readline())
+for i in 0:n-1
+print(fibonacci(i)," ")
+end
+end

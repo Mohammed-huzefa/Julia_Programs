a01c83ff68acf756b42eef7849784b9f878dcdeb
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-05-29 11:14:57 +0530

    Update 6a.jl

--- 6a.jl
+++ 6a.jl
@@ -1,20 +1,19 @@
 function freq_letter(text::String)
-    freq = Dict{Char, Int}()
+freq=Dict{Char,Int}()
+text=lowercase(text)
 
-    text = lowercase(text)
+for char in text
+if 'a'<=char<='z'
+freq[char]=get(freq,char,0)+1
+end
+end
 
-    for char in text
-        if 'a' <= char <= 'z'
-            freq[char] = get(freq, char, 0) + 1
-        end
-    end
-
-    for char in 'a':'z'
-        count = get(freq, char, 0)
-        println("$char:$count")
-    end
+for char in 'a':'z'
+count=get(freq,char,0)
+println("$char:$count")
+end
 end
 
 println("Enter a text:")
-text = readline()
+text=readline()
 freq_letter(text)

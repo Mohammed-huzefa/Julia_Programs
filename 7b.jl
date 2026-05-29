f912fe7821885292af3c942e7d3ae3ed6e9527ed
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-05-29 12:24:33 +0530

    Create 7b.jl

---
+++ 7b.jl
@@ -0,0 +1,21 @@
+file_path = "C:/Users/ushah/OneDrive/Desktop/aiml.txt"
+
+file_contents = read(file_path, String)
+
+words = split(lowercase(file_contents), r"\W+")
+
+unique_words = Set{String}()
+
+for word in words
+if !isempty(word)
+push!(unique_words, word)
+end
+end
+
+sorted_words = sort(collect(unique_words))
+
+println("Unique words found in the file:")
+
+for word in sorted_words
+println(word)
+end

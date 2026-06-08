c1aaf534f294d936538dda109509a9e03399438d
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-06-08 09:47:25 +0530

    Update 10a.jl

--- 10a.jl
+++ 10a.jl
@@ -1,16 +1,13 @@
 begin
 using Plots
-
-x = 0:0.01:2pi
-
-y = sin.(x) .+ sin.(2x)
-
-println("Displaying the plot of y = sin(x) + sin(2x)")
-
-plot(x, y,
-xlabel = "x",
-ylabel = "y",
-title = "Plot of y = sin(x) + sin(2x)",
-label = "y = sin(x) + sin(2x)",
-legend = :topright)
+x= -10:0.1:10
+y=x.^2
+Plots.plot(
+x,y,
+xlabel="x",
+ylabel="y",
+title="Plot of y = x^2",
+label = "y=x^2",
+legend = :topright
+)
 end
